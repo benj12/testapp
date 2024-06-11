a5e5d9b084e87f6ad72902b59f30abce1f306868
benj12 <44120311+benj12@users.noreply.github.com>
2024-06-11 17:10:01 -0400

    Edited imports for local-notification-service.dart

--- lib/services/local_notification_service.dart
+++ lib/services/local_notification_service.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:rxdart/subjects.dart';
-import 'package:timezone/timezone.dart';
-import 'package:timezone/data/latest.dart';
+import 'package:timezone/timezone.dart' as tz;
+import 'package:timezone/data/latest.dart' as tz;
 
 class LocalNotificationService {
   LocalNotificationService();
