c8b4a89679cd1ebec28bf60039075d3182d25f3f
benj12 <44120311+benj12@users.noreply.github.com>
2024-06-12 15:29:23 -0400

    Push notification working for iOS

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:testapp/services/local_notification_service.dart';
+
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = 
+  FlutterLocalNotificationsPlugin();
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,31 +12,67 @@ class HomeScreen extends StatefulWidget {
 }
 
 class HomeScreenState extends State<HomeScreen> {
+  
   @override
-  Widget build(BuildContext context) => Scaffold(
-    body: Center(
-      child: Column(
-        children: <Widget>[
-          const Padding(  
-            padding: EdgeInsets.only(top: 105),
-          ),
-          ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20),
-              ),
-              backgroundColor: Colors.red,
-              foregroundColor: Colors.black,
-            ),
-            onPressed: () async {
+  void initState () {
+    super.initState();
+    LocalNotificationService.initialize(flutterLocalNotificationsPlugin);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Virtues app'),
+      ),
+      body: Center(
+        child: SizedBox(
+          width: 180,
+          height: 50,
+          child: ElevatedButton(
+            onPressed: () {
+              LocalNotificationService.showBigTextNotification(
+                title: 'New message title', 
+                body: 'body', 
+                flutterLocalNotificationsPlugin: flutterLocalNotificationsPlugin
+              );
 
             },
             child: const Text(
-              'Send push notification'
+              'Send push notification',
+              style: TextStyle(
+                fontSize: 18,
+              ),
             ),
-          ),
-        ],
+          )
+        ),
       ),
-    ),
-  );
+    );
+  }
+  // Widget build(BuildContext context) => Scaffold(
+  //   body: Center(
+  //     child: Column(
+  //       children: <Widget>[
+  //         const Padding(  
+  //           padding: EdgeInsets.only(top: 105),
+  //         ),
+  //         ElevatedButton(
+  //           style: ElevatedButton.styleFrom(
+  //             shape: RoundedRectangleBorder(
+  //               borderRadius: BorderRadius.circular(20),
+  //             ),
+  //             backgroundColor: Colors.red,
+  //             foregroundColor: Colors.black,
+  //           ),
+  //           onPressed: () async {
+
+  //           },
+  //           child: const Text(
+  //             'Send push notification'
+  //           ),
+  //         ),
+  //       ],
+  //     ),
+  //   ),
+  // );
 }
